e8d9197f791052d83a103b6df6d8a618d15effba
Ranjith Dasthoti <dranjith2012@gmail.com>
2019-06-27 13:32:18 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   //place your moongodb atlas uri here
   mongoURI:
-    "mongodb+srv://Ranjith8:Ranjith8..@practicecluster-2b9h7.mongodb.net/test-interview?retryWrites=true&w=majority"
+    "mongodb+srv://<username>:<password>@practicecluster-2b9h7.mongodb.net/test-interview?retryWrites=true&w=majority"
 };
